ebf1b3f917e3b655b441f8df419b22c4f42985e4
stamatis <stamatiskourkoutas@gmail.com>
2020-04-22 22:32:42 +0300

    try catch added

--- app.js
+++ app.js
@@ -23,23 +23,27 @@ io.on("connection", (clientSocket) => {
   clientSocket.on("disconnecting", () => {
     console.log("Client:", clientSocket.id, "disconnecting from server")
 
-    Object.keys(clientSocket.rooms).forEach(roomName => {
-      if(clientSocket.id!=roomName){
-        // Delete from room
-        myRooms[roomName].removeClient(clientSocket.id);
-        
-        // Notify everyone in the room about all others
-        console.log(myRooms[roomName].clients)
-        if(Object.keys(myRooms[roomName].clients).length>0){
-          io.sockets.in(roomName).emit("ClientsUpdate", myRooms[roomName].clients);
-        }else{
-          delete myRooms[roomName]
-        }
+    try {
+      Object.keys(clientSocket.rooms).forEach(roomName => {
+        if(clientSocket.id!=roomName){
+          // Delete from room
+          myRooms[roomName].removeClient(clientSocket.id);
+          
+          // Notify everyone in the room about all others
+          console.log(myRooms[roomName].clients)
+          if(Object.keys(myRooms[roomName].clients).length>0){
+            io.sockets.in(roomName).emit("ClientsUpdate", myRooms[roomName].clients);
+          }else{
+            delete myRooms[roomName]
+          }
 
-        //clients = Object.keys(io.sockets.adapter.rooms[roomName].sockets);
-        //clients2 = clients.filter(client => client!==clientSocket.id);     
-      };
-    });
+          //clients = Object.keys(io.sockets.adapter.rooms[roomName].sockets);
+          //clients2 = clients.filter(client => client!==clientSocket.id);     
+        };
+      });
+    } catch (error) {
+      console.log("Error 1")
+    }
   });
 
   clientSocket.on("disconnect", () => {
@@ -48,70 +52,85 @@ io.on("connection", (clientSocket) => {
 
   clientSocket.on("JoinRoom", (roomName, language) => {
     console.log("Client:", clientSocket.id, "joined room:", roomName);
+    try{
+      // Create room for first client
+      if (!(roomName in myRooms)){
+        myRooms[roomName] = new Room(roomName, language);
+      }
 
-    // Create room for first client
-    if (!(roomName in myRooms)){
-      myRooms[roomName] = new Room(roomName, language);
+      // Join room
+      clientSocket.join(roomName);
+      var usr = "";
+      if(clientSocket.id in users){
+        usr = users[clientSocket.id]
+      }else{
+        usr = "Guest_"+(Math.floor(Math.random() * 99)+1);
+      }
+      myRooms[roomName].addClient(clientSocket.id, usr);
+
+      // Notify everyone in the room about all others
+      console.log(myRooms[roomName].clients)
+      io.sockets.in(roomName).emit("ClientsUpdate", myRooms[roomName].clients);
+      io.sockets.in(roomName).emit("BoardUpdate", myRooms[roomName].wordList);//
+    } catch (error) {
+      console.log("Error 2")
     }
-
-    // Join room
-    clientSocket.join(roomName);
-    var usr = "";
-    if(clientSocket.id in users){
-      usr = users[clientSocket.id]
-    }else{
-      usr = "Guest_"+(Math.floor(Math.random() * 99)+1);
-    }
-    myRooms[roomName].addClient(clientSocket.id, usr);
-
-    // Notify everyone in the room about all others
-    console.log(myRooms[roomName].clients)
-    io.sockets.in(roomName).emit("ClientsUpdate", myRooms[roomName].clients);
-    io.sockets.in(roomName).emit("BoardUpdate", myRooms[roomName].wordList);//
   });
 
   clientSocket.on("LeaveRoom", (roomName) => {
     console.log("Client:", clientSocket.id, "left room:", roomName);
+    try{
+      // Leave room and delete from room
+      clientSocket.leave(roomName);
+      myRooms[roomName].removeClient(clientSocket.id);
 
-    // Leave room and delete from room
-    clientSocket.leave(roomName);
-    myRooms[roomName].removeClient(clientSocket.id);
-
-    // Notify everyone in the room about all others
-    console.log(myRooms[roomName].clients)
-    if(Object.keys(myRooms[roomName].clients).length>0){
-      io.sockets.in(roomName).emit("ClientsUpdate", myRooms[roomName].clients);
-    }else{
-      delete myRooms[roomName]
+      // Notify everyone in the room about all others
+      console.log(myRooms[roomName].clients)
+      if(Object.keys(myRooms[roomName].clients).length>0){
+        io.sockets.in(roomName).emit("ClientsUpdate", myRooms[roomName].clients);
+      }else{
+        delete myRooms[roomName]
+      }
+    } catch (error) {
+      console.log("Error 3")
     }
   });
 
   clientSocket.on("SpymasterChange", (clients,roomName) => {
     console.log("Client:", clientSocket.id, "changed spymaster in:", roomName);
+    try{
+      myRooms[roomName].clients = clients;
 
-    myRooms[roomName].clients = clients;
-
-    // Notify everyone in the room about all others
-    console.log(myRooms[roomName].clients)
-    io.sockets.in(roomName).emit("ClientsUpdate", myRooms[roomName].clients);
+      // Notify everyone in the room about all others
+      console.log(myRooms[roomName].clients)
+      io.sockets.in(roomName).emit("ClientsUpdate", myRooms[roomName].clients);
+    } catch (error) {
+      console.log("Error 4")
+    }
   });
 
   clientSocket.on("NewGame", (roomName, language) => {
     console.log("Client:", clientSocket.id, "started new game in:", roomName);
-
+    try{
     myRooms[roomName].newGame(language);
 
     // Notify everyone in the room about change
     io.sockets.in(roomName).emit("BoardUpdate", myRooms[roomName].wordList);
+    } catch (error) {
+      console.log("Error 5")
+    }
   });
 
   clientSocket.on("WordListChange", (id,roomName) => {
-    console.log("Client:", clientSocket.id, "oppened word", id, "in:", roomName);
+    console.log("Client:", clientSocket.id, "opened word", id, "in:", roomName);
+    try{
+      myRooms[roomName].openCard(id);
 
-    myRooms[roomName].openCard(id);
-
-    // Notify everyone in the room about change
-    io.sockets.in(roomName).emit("BoardUpdate", myRooms[roomName].wordList);
+      // Notify everyone in the room about change
+      io.sockets.in(roomName).emit("BoardUpdate", myRooms[roomName].wordList);
+    } catch (error) {
+      console.log("Error 6")
+    }
   });
 
   clientSocket.on("ChangeUsername", (usr) => {
@@ -121,12 +140,15 @@ io.on("connection", (clientSocket) => {
 
   clientSocket.on("MsgChange", (roomName, username, message) => {
     console.log("Client:", clientSocket.id, "sent mesage in:", roomName);
+    try{
+      myRooms[roomName].addMessage(username, message);
 
-    myRooms[roomName].addMessage(username, message);
-
-    // Notify everyone in the room about change
-    console.log(myRooms[roomName].msgList)
-    io.sockets.in(roomName).emit("MsgUpdate", myRooms[roomName].msgList);
+      // Notify everyone in the room about change
+      console.log(myRooms[roomName].msgList)
+      io.sockets.in(roomName).emit("MsgUpdate", myRooms[roomName].msgList);
+    } catch (error) {
+      console.log("Error 7")
+    }
   });
 });
 
